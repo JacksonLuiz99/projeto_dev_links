1f3165fb031606cf21343abd84af36ba512b1a92
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2023-06-22 17:22:44 -0400

    : initianl commit

---
+++ .vscode/script.js
@@ -0,0 +1,16 @@
+function toggleMode(){
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag imag
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+ if(html.classList.contains('light')) {
+  //se tiver light mode, adicionar a imagem light
+  img.setAttribute("src", "./assets/avatar-light.jpg")
+} else {
+  //se tiver sem light mode, manter imagem normal
+  img.setAttribute("src", "./assets/avatr.png")
+}
+}
